ed00dc5522627f69fc5e45814ebc1a5d058ee1a9
JavaToster <kamil.gizatullin.03@gmail.com>
2024-06-10 13:42:57 +0300

    fix: скрыл приватные ключи

--- src/main/java/com/example/Messenger/MessengerApplication.java
+++ src/main/java/com/example/Messenger/MessengerApplication.java
@@ -2,6 +2,7 @@ package com.example.Messenger;
 
 import com.cloudinary.Cloudinary;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,10 +13,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
-import java.util.Properties;
 
 @SpringBootApplication
 public class MessengerApplication {
+
+	@Value("${cloudinary.cloud.name}")
+	private String cloudName;
+	@Value("${cloudinary.api_key}")
+	private String apiKey;
+	@Value("${cloudinary.api_secret}")
+	private String apiSecret;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MessengerApplication.class, args);
 	}
@@ -35,9 +43,9 @@ public class MessengerApplication {
 	@Bean
 	public Cloudinary cloudinary(){
 		return new Cloudinary(Map.of(
-				"cloud_name", "dyxjtpcdu",
-				"api_key", "598588667712299",
-				"api_secret", "6ryph5s7fUyG2JoXPBTQz0C9vPg"
+				"cloud_name", cloudName,
+				"api_key", apiKey,
+				"api_secret", apiSecret
 		));
 	}
 
